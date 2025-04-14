cdb51b53f4b5fe381bca399e7525b3bcc392f43c
Suyash Nikam <_itssuyash_@Suyashs-MacBook-Air.local>
2025-04-14 22:33:09 +0530

    updating routes

--- pizza_routes.py
+++ pizza_routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException, Depends, status, Header
 from sqlalchemy.orm import Session
 import models, schemas, database
+from fastapi_jwt_auth import AuthJWT
 import requests
 from typing import Optional
 from fastapi.encoders import jsonable_encoder
@@ -15,8 +16,18 @@ pizza_router = APIRouter(prefix="/pizza", tags=["pizza"])
 async def create_pizza(
         pizza: schemas.PizzaCreate,
         db: Session = Depends(database.get_db),
+        Authorize: AuthJWT = Depends(),
         Authorization: Optional[str] = Header(None)
 ):
+    try:
+        Authorize.jwt_required()
+        role = Authorize.get_raw_jwt().get("role")
+    except Exception:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    if role not in ["ADMIN", "STAFF"]:
+        raise HTTPException(status_code=403, detail="Only Admin and Staff can create pizzas")
+
     existing_pizza = db.query(models.Pizza).filter(models.Pizza.name == pizza.name).first()
     if existing_pizza:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Pizza already exists")
@@ -97,10 +108,21 @@ async def update_pizza(
     pizza_id: int,
     pizza_data: schemas.PizzaUpdate,
     db: Session = Depends(database.get_db),
-    authorization: Optional[str] = Header(None)
+    authorization: Optional[str] = Header(None),
+    Authorize: AuthJWT = Depends(),
+
 ):
     if not authorization:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authorization token required")
+
+    try:
+        Authorize.jwt_required()
+        role = Authorize.get_raw_jwt().get("role")
+    except Exception:
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
+
+    if role not in ["ADMIN", "STAFF"]:
+        raise HTTPException(status_code=403, detail="Only Admin and Staff can update pizzas")
 
     pizza = db.query(models.Pizza).filter(models.Pizza.id == pizza_id).first()
     if not pizza:
@@ -132,10 +154,20 @@ async def update_pizza(
 async def delete_pizza(
     pizza_id: int,
     db: Session = Depends(database.get_db),
-    authorization: Optional[str] = Header(None)
+    authorization: Optional[str] = Header(None),
+    Authorize: AuthJWT = Depends(),
 ):
     if not authorization:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authorization token required")
+
+    try:
+        Authorize.jwt_required()
+        role = Authorize.get_raw_jwt().get("role")
+    except Exception:
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
+
+    if role not in ["ADMIN", "STAFF"]:
+        raise HTTPException(status_code=403, detail="Only Admin and Staff can Delete pizzas")
 
     pizza = db.query(models.Pizza).filter(models.Pizza.id == pizza_id).first()
     if not pizza:
@@ -151,10 +183,27 @@ async def delete_pizza(
 async def get_pizzas_for_outlet(
         outlet_code: str,
         db: Session = Depends(database.get_db),
-        Authorization: Optional[str] = Header(None)
+        Authorization: Optional[str] = Header(None),
+        Authorize: AuthJWT = Depends()
 ):
     print(outlet_code)
     print(Authorization)
+
+    try:
+        Authorize.jwt_required()
+        role = Authorize.get_raw_jwt().get("role")
+    except Exception:
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
+
+    outlet_service_url = os.getenv("OUTLET_SERVICE_BASE_URL",
+                                   "http://127.0.0.1:8003") + f"/outlet/by-code/{outlet_code}"
+    try:
+        headers = {"Authorization": Authorization}
+        response = requests.get(outlet_service_url, headers=headers, timeout=5)
+        if response.status_code != 200:
+            raise HTTPException(status_code=404, detail=f"Outlet with code '{outlet_code}' not found")
+    except requests.exceptions.RequestException:
+        raise HTTPException(status_code=503, detail="Failed to communicate with outlet service")
     pizzas = db.query(models.Pizza).filter(
         (models.Pizza.outlet_code == outlet_code) | (models.Pizza.outlet_code.is_(None))
     ).all()
